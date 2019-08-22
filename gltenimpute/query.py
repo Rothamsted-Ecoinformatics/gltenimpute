1246f54106ee9ec411bcdb19bfe34a31dfa333b7
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2019-08-22 17:10:17 +0100

    meteostat added

--- gltenimpute/query.py
+++ gltenimpute/query.py
@@ -15,5 +15,6 @@ print("hello")
 #resp = requests.get('https://landgisapi.opengeohub.org/query/point?lat='+str(args.lat)+'&lon='+str(args.lon)+'&coll=predicted250m&regex=sol_grtgroup_usda.soiltax_c_250m_b0..0cm_1950..2017_v0.1.tif')
 
 #https://rest.soilgrids.org/query.html
+#https://api.meteostat.net/
 resp = requests.get('https://rest.soilgrids.org/query?lat='+str(args.lat)+'&lon='+str(args.lon)+'&attributes=TAXNWRB') 
 print(json.dumps(resp.json(), indent=4, sort_keys=True))
